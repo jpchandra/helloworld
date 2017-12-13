d5398d2bcfc77d76192731b4c5a7b596f312cd9b
phani <phanichandraj@gmail.com>
2017-12-13 07:17:14 -0500

    creating master branch

---
+++ src/main/java/com/manthani/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.manthani.example;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Initila GIT to GIT to run git Diff after adding ----This is example for the GIT Repository");
+	}
+
+}
